2e48d2f015af805586500b4d8a8c7603a163ea35
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-08 23:08:53 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,22 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:5000/posts';
+const API = axios.create({
+  baseURL: 'http://localhost:5000'
+});
 
-export const fetchPosts = () => axios.get(URL);
-export const createPost = (newPost) => axios.post(URL, newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${URL}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${URL}/${id}`);
-export const likePost = (id) => axios.patch(`${URL}/${id}/likepost`);
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem('profile');
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
+
+export const fetchPosts = () => API.get('/posts');
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likepost`);
+
+export const signIn = (formData) => API.post('/users/signin', formData);
+export const signUp = (formData) => API.post('/users/signup', formData);
