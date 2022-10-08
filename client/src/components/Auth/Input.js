edeeb15ea2763780ea81698568e97d23d00d264f
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-08 23:08:57 +0530

    Update Input.js

--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -3,20 +3,21 @@ import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
-const Input = ({ autoFocus, handleChange, label, name, half, type, handleShowPassword }) => {
+const Input = ({ autoFocus, handleChange, label, name, half, type, handleShowPassword, helperText }) => {
 
   return (
     <Grid item xs={12} sm={half ? 6 : 12}>
       <TextField
         name={name}
         label={label}
-        handleChange={handleChange}
+        onChange={handleChange}
         autoFocus={autoFocus}
         xs={6}
         variant="outlined"
         required
         type={type}
         fullWidth
+        helperText={helperText}
         InputProps={(name === 'password')
           ? { endAdornment: (
             <InputAdornment position="end">
