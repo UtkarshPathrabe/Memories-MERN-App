38a6d2ea49439d7f9a6f9aebea2e00f661ad9ac9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-08 19:23:35 +0530

    Create Input.js

---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+const Input = ({ autoFocus, handleChange, label, name, half, type, handleShowPassword }) => {
+
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+      <TextField
+        name={name}
+        label={label}
+        handleChange={handleChange}
+        autoFocus={autoFocus}
+        xs={6}
+        variant="outlined"
+        required
+        type={type}
+        fullWidth
+        InputProps={(name === 'password')
+          ? { endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={handleShowPassword}>
+                {(type === 'password') ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          )} : null}
+      />
+    </Grid>
+  );
+};
+
+export default Input;
