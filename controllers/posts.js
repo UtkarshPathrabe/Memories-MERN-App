9372ef5c796c87fdf2cd2cde23c367930004f3dc
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-09 15:47:25 +0530

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,26 @@
 import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
+const LIMIT = 6;
+
 export const getPosts = async (req, res) => {
+  const { page } = req.query;
   try {
-    const postMessages = await PostMessage.find();
-    res.status(200).json(postMessages);
+    const startIndex = (Number(page) - 1) * LIMIT;
+    const total = await PostMessage.countDocuments({});
+    const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+    res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT)});
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery, tags, page } = req.query;
+  try {
+    const title = new RegExp(searchQuery, "i");
+    const posts = await PostMessage.find({ $or: [{ title: title }, { tags: { $in: tags.split(',') } }]});
+    res.status(200).json({ data: posts, currentPage: 1, numberOfPages: 1});
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
