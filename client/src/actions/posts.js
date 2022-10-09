dc1758b6a106947593b69e5aeaf381555e6bfca9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-09 15:46:53 +0530

    Update posts.js

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,29 +1,44 @@
 import * as api from '../api';
-import { CREATE, DELETE, FETCH_ALL, LOADING_POSTS, UPDATE } from '../constants/actionTypes';
+import { CREATE, DELETE, END_LOADING, FETCH_ALL, FETCH_BY_SEARCH, START_LOADING, UPDATE } from '../constants/actionTypes';
 
-export const getPosts = () => async (dispatch) => {
+export const getPosts = (page) => async (dispatch) => {
   try {
-    dispatch({ type: LOADING_POSTS });
-    const { data } = await api.fetchPosts();
+    dispatch({ type: START_LOADING });
+    const { data } = await api.fetchPosts(page);
     dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.error(error);
+  } finally {
+    dispatch({ type: END_LOADING });
+  }
+};
+
+export const getPostsBySearch = (searchQuery) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+    const { data } = await api.fetchPostsBySearch(searchQuery);
+    dispatch({ type: FETCH_BY_SEARCH, payload: data });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    dispatch({ type: END_LOADING });
   }
 };
 
 export const createPost = (newPost) => async (dispatch) => {
   try {
-    dispatch({ type: LOADING_POSTS });
+    dispatch({ type: START_LOADING });
     const { data } = await api.createPost(newPost);
     dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.error(error);
+  } finally {
+    dispatch({ type: END_LOADING });
   }
 };
 
 export const updatePost = (id, post) => async (dispatch) => {
   try {
-    dispatch({ type: LOADING_POSTS });
     const { data } = await api.updatePost(id, post);
     dispatch({ type: UPDATE, payload: data });
   } catch (error) {
@@ -33,11 +48,13 @@ export const updatePost = (id, post) => async (dispatch) => {
 
 export const deletePost = (id) => async (dispatch) => {
   try {
-    dispatch({ type: LOADING_POSTS });
+    dispatch({ type: START_LOADING });
     await api.deletePost(id);
     dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.error(error);
+  } finally {
+    dispatch({ type: END_LOADING });
   }
 };
 
